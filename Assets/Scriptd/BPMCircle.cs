6ff1a5354d0e4031e87c138b10b0ef6f0fc71095
naoto-leon <naoto.leon@gmail.com>
2019-08-28 18:14:59 +0900

    Initial commit

---
+++ Assets/Scriptd/BPMCircle.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPMCircle : MonoBehaviour
+{
+    public SerialHandler serialHandler;
+
+    [SerializeField]
+    float pospoX, pospoY;
+
+    public float speed;
+    public float radius;
+    public float zPosition;
+    float x;
+    float y;
+    float z;
+
+
+    private TrailRenderer _trailLenderer;
+
+    public float beat;
+    public float timer;
+    public float bpm;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = 3.75f;
+        radius = 16f;
+
+        zPosition = 0f;
+
+        _trailLenderer = GetComponent<TrailRenderer>();
+
+
+
+        //信号を受信したときに、そのメッセージの処理を行う
+        serialHandler.OnDataReceived += OnDataReceived;
+
+    }
+
+
+
+    void OnDataReceived(string message)
+    {
+
+        float bpnint;
+
+        if (float.TryParse(message, out bpnint))
+        {
+    
+            beat = bpnint;
+        }
+        else
+        {
+            //Debug.Log("数値に変換できません");
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        bpm = 60 / beat;
+        Debug.Log(bpm);
+
+        x = pospoX + (radius * Mathf.Sin(Time.time * speed));
+        z = zPosition;
+        y = pospoY + (radius * Mathf.Cos(Time.time * speed));
+
+        transform.position = new Vector3(x, y, z);
+
+
+        if (timer > bpm)
+        {
+            timer -= bpm;
+            radius = 8f;
+        }
+        else
+        {
+            radius = 16f;
+        }
+
+        timer += Time.deltaTime;
+
+    }
+
+
+}
---
+++ Assets/Scriptd/SerialLight.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+using uOSC;
+
+public class SerialLight : MonoBehaviour
+{
+
+    public SerialHandler serialHandler;
+    public Text text;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //信号を受信したときに、そのメッセージの処理を行う
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var client = GetComponent<uOscClient>();
+
+
+        //client.Send("/uOSC/test", message);
+        //client.Send("/uOSC/test", 10, "hoge", 1.234f, new byte[] { 1, 2, 3 });
+    }
+
+    /*
+	 * シリアルを受け取った時の処理
+	 */
+void OnDataReceived(string message)
+    {
+        var client = GetComponent<uOscClient>();
+        //client.Send("/uOSC/test", 10, "hoge", 1.234f, new byte[] { 1, 2, 3 });
+
+        //client.Send("/uOSC/test", message);
+
+        //var data = message.Split(
+        //   new string[] { "\t" }, System.StringSplitOptions.None);
+        //if (data.Length < 2) return;
+
+
+        int i;
+
+        if (int.TryParse(message, out i))
+        {
+            //Debug.Log(i);
+        }
+        else
+        {
+            //Debug.Log("数値に変換できません");
+        }
+
+        client.Send("/uOSC/test", i);
+
+        try
+        {
+
+            text.text = message; // シリアルの値をテキストに表示
+                                 //var angleX = float.Parse(data[0]);
+                                 //var angleY = float.Parse(data[1]);
+                                 //Debug.Log(data);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+
+       
+    }
+}
